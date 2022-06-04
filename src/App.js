1c3169ef1362b5ab5120dd92c05cb0de2e99c18a
manojAdev <manoj.a.agarwal@capgemini.com>
2022-06-04 09:27:15 +0530

    contact manger code with comments

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
-import React,{useState} from 'react';
-import MyList from './listComponent';
-const App=()=>{
+import React, { useState } from "react";
+import MyList from "./listComponent";
+const App = () => {
+  const [myM, setMYm] = useState("");
 
-const [myM,setMYm]=useState('');
+  const [managerListS, setmanagerListS] = useState([]);
 
-const [managerListS,setmanagerListS]=useState([])
-
-  //const managerList=[];
-
-  const inputManager=(e)=>{
-    let inV=e.target.value;
+  const inputManager = (e) => {
+    let inV = e.target.value;
     setMYm(inV);
-  }
+  };
 
-  const formSubmit=(e)=>{
-    //managerList.push(myM);
-    setmanagerListS([...managerListS,myM]);
+  const formSubmit = (e) => {
+    //merging old and new values into managerListS array
+    setmanagerListS([...managerListS, myM]);
     e.preventDefault();
-   
-  }
+  };
 
-
-return (<div>
-       <form onSubmit={formSubmit}>
-        <label>Enter Manager Name</label>: <input type="text" onChange={inputManager} />
+  return (
+    <div>
+      <form onSubmit={formSubmit}>
+        <label>Enter Manager Name</label>:{" "}
+        <input type="text" onChange={inputManager} />
         <input type="submit" value="Add Manager" />
-
-       </form>
-       <MyList data={managerListS} />
-
-</div>)
-
-}
+      </form>
+      <MyList data={managerListS} />
+    </div>
+  );
+};
 
 export default App;
